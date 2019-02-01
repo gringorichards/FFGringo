9a392947f3896a78e9be464ce7cb42d9879c1ba5
gringorichards <gringo.richards@gmail.com>
2019-02-01 20:11:52 +0000

    dfsdsf

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class CaptainsReport(models.Model):
+    manager_name = models.CharField(max_length=100)
+    points = models.IntegerField()
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import csv
 
 from .models import Greeting
 
@@ -7,7 +8,6 @@ from .models import Greeting
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
-
 
 def db(request):
 
