96996d0e0baae40ff0e16f83e6f5f95f65866817
gringorichards <43341145+gringorichards@users.noreply.github.com>
2019-03-07 17:52:49 +0000

    dd

--- startuplocal.bat
+++
@@ -1,2 +0,0 @@
-SET DATABASE_URL=postgres://dbmighejucqakd:ab32ff5982c759a528fee71496c61b14afdc0f9bfc4ea751d5eb7e1d133c5a35@ec2-54-217-208-105.eu-west-1.compute.amazonaws.com:5432/d8g2qukafrgsbi
-heroku local web -f Procfile.windows
